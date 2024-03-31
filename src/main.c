c9d79d5721afb2ab70ce587cc7e5f65f93a4e604
Eljily-Mohamed <hamadekj15@gmail.com>
2024-03-31 15:02:56 +0200

    read temp

--- src/main.c
+++ src/main.c
@@ -20,17 +20,5 @@ int main(void)
     uart_init(_USART2, 9600, UART_8N1, on_rx_cb);
     i2c_master_init(_I2C1);
     TH02_begin(); // Initialize TH02 sensor
-
-    while (1)
-    {
-        //temperature = TH02_ReadTemperature();
-        //humidity = TH02_ReadHumidity();
-
-        uart_printf(_USART2, "Temperature: %.2f C\r\n", temperature);
-        //uart_printf(_USART2, "Humidity: %.2f %%\r\n", humidity);
-
-        //delay_ms(1000); // Delay for 1 second
-    }
-
     return 0;
 }
