b1961ec9b05cb7ebe92978f3f43634b70d02d911
Eljily-Mohamed <hamadekj15@gmail.com>
2024-04-01 14:27:13 +0200

    mai

--- src/main.c
+++ src/main.c
@@ -3,13 +3,13 @@
 #include "lib/uart.h"
 #include "lib/util.h"
 #include "lib/i2c.h"
-#include "libshield/th02.h"
 
-#define MMA0
+#define COLORSENSOR
+
 
 volatile char cmd;
-char buf[10];
-
+#ifdef TEMP_HUMIDITYSENSOR
+#include "libshield/th02.h"
 static void on_rx_cb(char c)
 {
 	cmd = c;
@@ -31,3 +31,31 @@ int main(void)
 	}
 	return 0;
 }
+#endif
+
+#ifdef COLORSENSOR
+#include "libshield/tcs3414cs.h"
+static void on_rx_cb(char c)
+{
+    cmd = c;
+}
+
+int main(void)
+{
+	// Variables pour stocker les valeurs de couleur
+    uint16_t red, green, blue, clear;
+	uart_init(_USART2, 115200, UART_8N1, on_rx_cb);
+	i2c_master_init(_I2C1);
+	tcs3414cs_init();
+
+	
+	// Lecture des valeurs de couleur
+	tcs3414cs_read_color(&red, &green, &blue, &clear);
+	uart_printf(_USART2,"\n\rValeur rouge: %u\n", red);
+	uart_printf(_USART2,"\n\rValeur verte: %u\n", green);
+	uart_printf(_USART2,"\n\rValeur bleue: %u\n", blue);
+	uart_printf(_USART2,"\n\rValeur claire: %u\n", clear);
+	while(1) {}
+	return 0;
+}
+#endif
